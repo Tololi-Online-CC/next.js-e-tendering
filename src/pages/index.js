277b1c33f58fff390152aee41d20209b1d614faa
edilson zau <dannibuga@gmail.com>
2023-04-06 11:04:42 +0200

    Mobile banner glitch removed

--- src/pages/index.js
+++ src/pages/index.js
@@ -46,27 +46,29 @@ export default function Home() {
       )
     }
   }
-
-  
-  return (
-    <>
-      <Head>
-        <title>Create Next App</title>
-        <meta name="description" content="Namcor E-tendering online platform" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main className={styles.main}>
-        <div className='login'>
-          <form className="form" target="_blank" onSubmit={handleSubmit}>
-            <h2>LOGIN</h2>
-            <input type="email" name="Email" placeholder="Email" className="input" required /><br />
-            <input type="password" name="Password" placeholder="Password" className="input" required /><br />
-            <button className="btn" type="submit">Submit</button>
-          </form>
-          <Link href={'/register'} className='link' style={{ margin: 0 }}>Register</Link>
-        </div>
-      </main>
-    </>
-  )
+  if(windowSize.width) {
+    if(windowSize.width > 767){
+      return (
+        <>
+          <Head>
+            <title>Create Next App</title>
+            <meta name="description" content="Namcor E-tendering online platform" />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <link rel="icon" href="/favicon.ico" />
+          </Head>
+          <main className={styles.main}>
+            <div className='login'>
+              <form className="form" target="_blank" onSubmit={handleSubmit}>
+                <h2>LOGIN</h2>
+                <input type="email" name="Email" placeholder="Email" className="input" required /><br />
+                <input type="password" name="Password" placeholder="Password" className="input" required /><br />
+                <button className="btn" type="submit">Submit</button>
+              </form>
+              <Link href={'/register'} className='link' style={{ margin: 0 }}>Register</Link>
+            </div>
+          </main>
+        </>
+      )
+    }
+  }
 }
